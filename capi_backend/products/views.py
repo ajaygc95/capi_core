2021624719c762c7766d2f7f909247f3ebc0e50d
Ajay GC <ajaygc80@meta.com>
2024-07-03 16:57:08 -0700

    Webhooks Added

--- capi_backend/products/views.py
+++ capi_backend/products/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.views import APIView
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
